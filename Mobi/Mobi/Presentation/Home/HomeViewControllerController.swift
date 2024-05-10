4ab574ed67a53bdb686c3b0409ec0e6267df1c3f
6uohul <kiy010604@naver.com>
2024-05-10 18:54:13 +0900

    [Feat] 홈 VC에 CVC 반영

--- Mobi/Mobi/Presentation/Home/HomeViewControllerController.swift
+++ Mobi/Mobi/Presentation/Home/HomeViewControllerController.swift
@@ -14,21 +14,21 @@ class HomeViewController: UIViewController {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .clear
+        collectionView.backgroundColor = .black
         collectionView.isScrollEnabled = true
         collectionView.isPagingEnabled = true
         return collectionView
     }()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        registerCells()
+        setDelegate()
+        setLayout()
     }
 
     private func registerCells() {
-        collectionView.register(BlockedUserCVC.self, forCellWithReuseIdentifier: BlockedUserCVC.className)
+        collectionView.register(PlaceCollectionViewCell.self, forCellWithReuseIdentifier: PlaceCollectionViewCell.className)
     }
     
     private func setDelegate() {
@@ -36,5 +36,34 @@ class HomeViewController: UIViewController {
         collectionView.dataSource = self
     }
 
+    private func setLayout() {
+        view.backgroundColor = .white
+        view.addSubviews(collectionView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview() /// navigation 추가시 변경
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
 }
 
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return testDummy.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PlaceCollectionViewCell.className, for: indexPath) as? PlaceCollectionViewCell else { return  UICollectionViewCell() }
+        cell.configure(model: testDummy[indexPath.item])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 102)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+}
